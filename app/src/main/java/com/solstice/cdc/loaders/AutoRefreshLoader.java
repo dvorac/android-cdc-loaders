629cba170905efdd16fc65e26c48a6e9ff03ccc9
dustin.vorac <dvorac@solstice-consulting.com>
2014-08-21 11:19:13 -0500

    comments

--- app/src/main/java/com/solstice/cdc/loaders/AutoRefreshLoader.java
+++ app/src/main/java/com/solstice/cdc/loaders/AutoRefreshLoader.java
@@ -3,6 +3,13 @@ package com.solstice.cdc.loaders;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 
+/**
+ * all we know about autorefreshloader...
+ * how this expands on asynctaskloader...
+ * examples where it may be useful...
+ * gotchas specific to autorefreshloader...
+ * @param <T>
+ */
 public class AutoRefreshLoader<T> extends AsyncTaskLoader<T> {
     public AutoRefreshLoader(Context context) {
         super(context);
