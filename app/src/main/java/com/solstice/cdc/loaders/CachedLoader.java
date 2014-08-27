e6cc9d0bc86fc6cbbeb2e87a0f90035175537235
Fola Dada <fdada@solstice-consulting.com>
2014-08-27 11:14:16 -0500

    cachedLoader

--- app/src/main/java/com/solstice/cdc/loaders/CachedLoader.java
+++ app/src/main/java/com/solstice/cdc/loaders/CachedLoader.java
@@ -6,10 +6,49 @@ import android.content.Context;
 public class CachedLoader<T> extends AsyncTaskLoader<T> {
 
     // TODO : CachedLoader base implementation. All you, Fola :)
+    private T mResult;
+
 
     public CachedLoader(Context context) {
         super(context);
     }
+
+    @Override
+    protected void onStartLoading(){
+        if(mResult !=null) {
+            //if we currently have a result available, deliver it
+            deliverResult(mResult);
+        }
+        if(takeContentChanged() || mResult == null){
+            //if the data has changed since the last time it load
+            //or is not currently available, start load.
+            forceLoad();
+        }
+    }
+    @Override
+    protected void onStopLoading(){
+        //Attempt to cancel the current load task if possible
+        cancelLoad();
+    }
+
+    @Override
+    protected void onReset(){
+        super.onReset();
+
+        onStartLoading();
+        mResult = null;
+    }
+
+    @Override
+    public void deliverResult(T data){
+        mResult = data;
+        if(isStarted()){
+            //if the Loader is currently started, we can immediately
+            //deliver its results
+            super.deliverResult(data);
+        }
+    }
+
 
     @Override
     public T loadInBackground() {
